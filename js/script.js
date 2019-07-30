6e63e77556a26653a2df94d40403c6ba3fdfc24b
jacekskowalski <jsk_profil@hotmail.com>
2019-07-30 11:24:33 +0200

    add files

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+navBarToggle.addEventListener('click',  () =>{
+        mainNav.classList.toggle('active');
+});
