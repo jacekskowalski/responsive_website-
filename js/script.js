7429d7dc60a59ae98a7c5574153fccbd795e9431
jacekskowalski <jsk_profil@hotmail.com>
2019-08-07 22:50:34 +0200

    add menu and carousel

--- js/script.js
+++ js/script.js
@@ -1,5 +1,36 @@
+
+let slideIndex = 1;
+showSlides(slideIndex);
 let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
-navBarToggle.addEventListener('click',  () =>{
-        mainNav.classList.toggle('active');
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+ let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dotBlack");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" activeSlide", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " activeSlide";
+}
+
+
+
+navBarToggle.addEventListener('click', function () {
+
+    mainNav.classList.toggle('active');
 });
